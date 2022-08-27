d9402a1228c74f618fc11799011c0c4dde082b26
Anna Popova <panasenko-anna.8@mail.ru>
2022-08-27 18:19:28 +0300

    fix problems

--- src/__tests__/orderByProps.js
+++ src/__tests__/orderByProps.js
@@ -1,6 +1,6 @@
 import orderByProps from '../orderByProps';
 
-test('testing orderByProps function', () => {
+test('test1 orderByProps function', () => {
   const person = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
@@ -11,4 +11,28 @@ test('testing orderByProps function', () => {
     { key: 'attack', value: 80 },
     { key: 'defence', value: 40 },
     { key: 'health', value: 10 }]);
+});
+
+test('test2 orderByProps function', () => {
+  const person = {
+    name: 'John', lastName: 'Smith', age: 30, male: 'male', education: 'university education',
+  };
+  const result = orderByProps(person, ['male', 'education', 'name']);
+  expect(result).toEqual([
+    { key: 'name', value: 'John' },
+    { key: 'male', value: 'male' },
+    { key: 'education', value: 'university education' },
+    { key: 'age', value: 30 },
+    { key: 'lastName', value: 'Smith' }]);
+});
+
+test('test3 orderByProps function', () => {
+  const person = {
+    name: 'John', lastName: 'Smith', age: 30,
+  };
+  const result = orderByProps(person, []);
+  expect(result).toEqual([
+    { key: 'age', value: 30 },
+    { key: 'lastName', value: 'Smith' },
+    { key: 'name', value: 'John' }]);
 });
--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,19 +1,19 @@
-function orderByProps(obj, props) {
+export default function orderByProps(obj, props) {
   const propsArr = [];
   const sortArr = [];
 
   for (const key in obj) {
     if (props.includes(key)) {
-      propsArr.push({key, value: obj[key],});
+      propsArr.push({ key, value: obj[key] });
     } else {
-      sortArr.push({key, value: obj[key],});
+      sortArr.push({ key, value: obj[key] });
     }
   }
   // console.log(propsArr);
   // console.log(sortArr);
 
-  sortArr.sort((a, b) => a.key > b.key ? 1 : -1);
+  sortArr.sort((a, b) => (a.key > b.key ? 1 : -1));
   // console.log(sortArr);
-  
+
   return [...propsArr, ...sortArr];
 }
